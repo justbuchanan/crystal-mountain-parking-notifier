77f6126fffa78cf9212be59e22b2a3402877d854
Justin Buchanan <justbuchanan@gmail.com>
2025-02-08 15:40:18 -0800

    reformat code

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ import time
 import json
 import argparse
 
-parser = argparse.ArgumentParser(prog='parkbot', description='Check the crystal mountain parking website for availability for a given day')
-parser.add_argument('--day',type=int,required=True)
-parser.add_argument('--month',type=int,required=True)
-parser.add_argument('--year', type=int,default=2025)
+parser = argparse.ArgumentParser(
+    prog='parkbot',
+    description=
+    'Check the crystal mountain parking website for availability for a given day'
+)
+parser.add_argument('--day', type=int, required=True)
+parser.add_argument('--month', type=int, required=True)
+parser.add_argument('--year', type=int, default=2025)
 args = parser.parse_args()
 
 options = Options()
@@ -21,11 +25,14 @@ driver.get('https://parking.crystalmountainresort.com')
 # give it a sec for javascript to load
 time.sleep(1)
 
-cal_entries = driver.find_elements(By.CSS_SELECTOR, f'#calendar_{args.year}-{args.month:02d} .calendar_day')
+cal_entries = driver.find_elements(
+    By.CSS_SELECTOR, f'#calendar_{args.year}-{args.month:02d} .calendar_day')
 cal_entries = [entry for entry in cal_entries if entry.text == str(args.day)]
 
 if len(cal_entries) != 1:
-    print(f"unexpected html. found {len(cal)} matching calendar entries, wanted 1")
+    print(
+        f"unexpected html. found {len(cal)} matching calendar entries, wanted 1"
+    )
     exit(1)
 cal = cal_entries[0]
 data = json.loads(cal.get_attribute('data-json'))
